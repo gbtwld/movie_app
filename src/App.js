cb6a747a2a4808842c653885635686026f4384ed
dujaejeong <gbtwld@gmail.com>
2020-09-03 15:25:16 +0900

    Some practices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 
-function App() {
+function Food({ fav, sec }) {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <h3>
+      I love {fav} and {sec}!!
+    </h3>
   );
+}
+
+const foodILike = [
+  {
+    name: "Pizza",
+    beb: "Coke",
+  },
+  {
+    name: "Hamburger",
+    beb: "Soda",
+  },
+  {
+    name: "Chicken",
+    beb: "Beer",
+  },
+];
+
+function renderFood(menu) {
+  return <Food fav={menu.name} sec={menu.beb} />;
+}
+
+function App() {
+  return <div className="App">{foodILike.map(renderFood)}</div>;
 }
 
 export default App;
