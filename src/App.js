4b045497abfce66cda86f2313a202fd59d35152e
dujaejeong <gbtwld@gmail.com>
2020-09-04 16:02:16 +0900

    bkbbk

--- src/App.js
+++ src/App.js
@@ -1,79 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ fav, sec }) {
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {};
+  minus = () => {};
+
+  render() {
     return (
-        <h3>
-            I love {fav} and {sec}!!!!
-        </h3>
+      <div>
+        <h1>The number is {this.state.count}!</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
     );
-}
-
-function Friends({ name, age, firstMet }) {
-    return (
-        <h2>
-            {name} is {age} years old. {firstMet}.
-        </h2>
-    );
-}
-
-const foodILike = [
-    {
-        name: "Pizza",
-        beb: "Coke",
-    },
-    {
-        name: "Hamburger",
-        beb: "Soda",
-    },
-    {
-        name: "Chicken",
-        beb: "Beer",
-    },
-];
-
-const friendsList = [
-    {
-        name: "Lee",
-        age: 22,
-        firstMet: 7,
-    },
-    {
-        name: "Kang",
-        age: 21,
-        firstMet: 8,
-    },
-    {
-        name: "Kim",
-        age: 22,
-        firstMet: 8,
-    },
-    {
-        name: "Jeon",
-        age: 20,
-        firstMet: 1,
-    },
-    {
-        name: "Choi",
-        age: 22,
-        firstMet: 1,
-    },
-];
-
-function renderFood(menu) {
-    return <Food fav={menu.name} sec={menu.beb} />;
-}
-
-function renderFriends(friend) {
-    return <Friends name={friend.name} age={friend.age} firstMet={friend.firstMet} />;
-}
-
-function App() {
-    return (
-        <div className="App">
-            {foodILike.map(renderFood)}
-            {friendsList.map(renderFriends)}
-        </div>
-    );
+  }
 }
 
 export default App;
